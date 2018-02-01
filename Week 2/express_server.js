cf7c6f2a088674f6ab37f53d5d478edd26a393c7
Student User <studentuser@D-108-179-179-37.dhcp4.washington.edu>
2018-02-01 09:53:30 -0800

    Added Week 2 files

--- Week 2/express_server.js
+++ Week 2/express_server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendStatus(200);
+app.get('/api', (req, res) => {
+    res.send('Response to GET request');
     res.end();
-})
+});
+
+app.put('/', (req, res) => {
+    res.send('Response to PUT request');
+    res.end();
+});
+
+app.post('/', (req, res) => {
+    res.send('Response to POST request');
+    res.end();
+});
+
+app.delete('/', (req, res) => {
+    res.send('Response to DELETE request');
+    res.end();
+});
 
 app.listen(8080, () => {
     console.log('Listening on port 8080');
